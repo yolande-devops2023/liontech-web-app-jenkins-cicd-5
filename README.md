6faefed9bba8303103e3a487650e1638653b554a
liontechacademy <info@liontechacademy.com>
2022-11-07 19:28:17 -0500

    maven-sonarqube-project repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# web-applications
+welcome to this new experience. Please read the following instructions as you build and run your pipelines: 
+I and Engineer Gaelle Testing slack integrations
+
+Best practices- for pipelines:
+All pipelines must be named using this format. e.g prince-client-pipeline
+crone tabs for build triggers:  https://crontab-generator.org/
+thank you everyone
+More details to follow about our Agile approach 
+Starting a new build for Anu Systems
+JENKINS SONARQUNE INTEGRATION:
+THIS IS is done in the Global tool configuration tab in jenkins
+
+      jenkins -sonarqube integration 
+       visiting the pom.xml file in your project directory
+     and modify the following:
+      put in your password and user name.
+      server url. (please dont change the url)
---
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -0,0 +1,34 @@
+package com.mt.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeService {
+
+	
+	@RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
+	@ResponseBody
+	String uploadImage(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
+			throws JSONException {
+
+		JSONObject js = new JSONObject();
+		js.put("Name", "LION TECH ACADEMY");
+		js.put("Calling Name", "Lion ");
+		js.put("LION TECHACADEMY", "2020-08-21");
+		js.put("Frank", "1975-08-21");
+		js.put("Hobbies", "Reading Technical Blogs,Teaching, Changing lives..");
+		js.put("Places he like", "Africa, Church, His native place");
+                                        js.put("DevOps Modules", "Maven, Github/Git, Nexus, Sonarqube, Jenkins His native place");
+		return js.toString();
+}
+}
